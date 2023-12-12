3a5bd0923ed1a13eb6eac473a5b87b649aacd331
Elkin Aguilar <aguilar.elkin99@gmail.com>
2023-12-12 16:59:04 -0500

    change errot testing

--- app/src/test/java/com/kto/employenexa/ui/employe/EmployeViewModelTest.kt
+++ app/src/test/java/com/kto/employenexa/ui/employe/EmployeViewModelTest.kt
@@ -53,7 +53,7 @@ class EmployeViewModelTest {
             coVerify { getEmploye() }
             assertEquals(
                 viewModel.state.value,
-                EmployeState.Loading
+                EmployeState.Success(listOf(EmployeMotherObject.anyResponse.toDomain()))
             )
         }
     }
